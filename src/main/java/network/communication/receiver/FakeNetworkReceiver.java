70899968530471470111a6941048f05e42eda5b9
bogdanzmeul <bogdanzmeul@gmail.com>
2026-05-25 16:48:29 +0300

    Add fake network receiver and sender implementations

---
+++ src/main/java/network/communication/receiver/FakeNetworkReceiver.java
@@ -0,0 +1,50 @@
+package network.communication.receiver;
+
+import cipher.Encoder;
+import data.Message;
+import data.Package;
+import network.protocol.NetworkReceiver;
+import utils.MessageType;
+
+import java.util.Random;
+import java.util.concurrent.atomic.AtomicLong;
+
+public class FakeNetworkReceiver implements NetworkReceiver {
+    private final Encoder encoder;
+    private final int messagesLimit;
+    private final Random random = new Random();
+    private final AtomicLong packetId = new AtomicLong(1);
+    private int generatedMessages;
+
+    public FakeNetworkReceiver(Encoder encoder, int messagesLimit) {
+        this.encoder = encoder;
+        this.messagesLimit = messagesLimit;
+    }
+
+    @Override
+    public synchronized byte[] receiveMessage() throws Exception {
+        if (generatedMessages >= messagesLimit) {
+            return null;
+        }
+
+        generatedMessages++;
+
+        int type = random.nextInt(6) + 1;
+        String text = createMessage(type);
+        Package pack = new Package((byte) 1, packetId.getAndIncrement(), new Message(type, 1, text));
+
+        return encoder.encode(pack);
+    }
+
+    private String createMessage(int type) {
+        return switch (type) {
+            case MessageType.GET_PRODUCT_COUNT -> "bananas";
+            case MessageType.TAKE_PRODUCT -> "bananas;1";
+            case MessageType.ADD_PRODUCT -> "bananas;2";
+            case MessageType.ADD_GROUP -> "fruit";
+            case MessageType.ADD_PRODUCT_TO_GROUP -> "fruit;bananas";
+            case MessageType.SET_PRICE -> "bananas;71.5";
+            default -> "";
+        };
+    }
+}
