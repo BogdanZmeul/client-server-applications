6a645c1e9146ac8437a770c8a858343e48fec3a8
bogdanzmeul <bogdanzmeul@gmail.com>
2026-05-25 23:27:26 +0300

    Add security, processor, and storage components

---
+++ src/main/java/network/security/Decriptor.java
@@ -0,0 +1,45 @@
+package network.security;
+
+import cipher.Decoder;
+import data.Package;
+
+import java.util.concurrent.BlockingQueue;
+
+public class Decriptor implements Runnable {
+    private final Decoder decoder;
+    private final BlockingQueue<byte[]> input;
+    private final BlockingQueue<Package> output;
+    private final byte[] endPacket;
+
+    public Decriptor(Decoder decoder, BlockingQueue<byte[]> input,
+                     BlockingQueue<Package> output, byte[] endPacket) {
+        this.decoder = decoder;
+        this.input = input;
+        this.output = output;
+        this.endPacket = endPacket;
+    }
+
+    @Override
+    public void run() {
+        try {
+            while (true) {
+                byte[] data = input.take();
+                if (data == endPacket) {
+                    break;
+                }
+
+                decrypt(data);
+            }
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+    }
+
+    public void decrypt(byte[] message) {
+        try {
+            output.put(decoder.decode(message));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
