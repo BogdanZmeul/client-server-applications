c5ca4b0bc117d719bfca80dd7888e37aab3cb06d
bogdanzmeul <bogdanzmeul@gmail.com>
2026-05-30 22:29:18 +0300

    Add TCP sender

---
+++ src/main/java/network/tcp/TcpNetworkSender.java
@@ -0,0 +1,30 @@
+package network.tcp;
+
+import network.protocol.NetworkSender;
+
+import java.io.OutputStream;
+import java.net.InetAddress;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+public class TcpNetworkSender implements NetworkSender {
+    private final OutputStream output;
+    private final AtomicBoolean closed = new AtomicBoolean(false);
+
+    public TcpNetworkSender(OutputStream output) {
+        this.output = output;
+    }
+
+    @Override
+    public synchronized void sendMessage(byte[] data, InetAddress target) throws Exception {
+        if (closed.get()) {
+            return;
+        }
+
+        output.write(data);
+        output.flush();
+    }
+
+    public void close() {
+        closed.set(true);
+    }
+}
